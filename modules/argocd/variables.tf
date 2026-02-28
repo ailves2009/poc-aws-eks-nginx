2ce5b2af05e37df1f0c80ee152300be6ad6437e0
Alexander Ilves <ailves2009@gmail.com>
2026-02-28 22:17:36 +0100

    remove unused modules

--- modules/argocd/variables.tf
+++
@@ -1,6 +0,0 @@
-# /modules/argocd/argocd.tf
-
-variable "cluster_name" {
-  description = "EKS cluster name"
-  type        = string
-}
--- modules/monitoring/outputs.tf
+++
@@ -1,6 +0,0 @@
-// /modules/monitoring/outputs.tf
-
-output "sns_topic_arn" {
-  description = "ARN of the SNS topic used for CPU alarms"
-  value       = aws_sns_topic.cpu_alarms.arn
-}
